948b86a11c8f9f6db6a041f6404344eaeb441bc4
Xavier Julián <xavier.julian@kaleidos.net>
2014-06-20 13:30:55 +0200

    Fixed projects page captures and structure

--- app/scripts/modules/media.js
+++ app/scripts/modules/media.js
@@ -13,6 +13,7 @@ app.view('media', ['$el'], function ($el) {
     function isVisible() {
         var visibility = isScrolledIntoView(video);
         if(visibility) {
+            video.prop("volume", 0.2);
             video[0].play();
         } else {
             video[0].pause();
@@ -22,32 +23,4 @@ app.view('media', ['$el'], function ($el) {
     function isScrolledIntoView(elem) {
         return $(elem).is_on_screen();
     }
-
-    $.fn.is_on_screen = function(){
-        var win = $(window);
-        var viewport = {
-            top : win.scrollTop(),
-            left : win.scrollLeft()
-        };
-        viewport.right = viewport.left + win.width();
-        viewport.bottom = viewport.top + win.height();
-
-        var bounds = this.offset();
-        bounds.right = bounds.left + this.outerWidth();
-        bounds.bottom = bounds.top + this.outerHeight();
-
-        return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
-    };
-
-
-//     $.fn.is_on_screen = function(){
-//         var win = $(window);
-//         var top = win.scrollTop();
-//         var blur = 200;
-//         var bottom = top + win.height();
-//         var bounds = this.offset();
-
-//         return (!(bottom < bounds.top || top > bounds.bottom));
-//     };
-
 });
