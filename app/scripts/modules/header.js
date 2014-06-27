f78ffe1f7ff8614d5193a09bdc8fad488ab40928
Xavier Julián <xavier.julian@kaleidos.net>
2014-06-27 12:51:45 +0200

    Participation

--- app/scripts/modules/header.js
+++ app/scripts/modules/header.js
@@ -4,6 +4,8 @@ app.view('header', ['$el'], function ($el) {
     //Vars
     var siblingSection = $el.next();
     var learnMore = $el.find('.learn-more');
+    var participate = $el.find('.participate');
+    var participateGoal = $el.siblings('.participation');
 
     //Events
     $(window).on('resize',function() {
@@ -13,6 +15,11 @@ app.view('header', ['$el'], function ($el) {
     learnMore.on('click' ,function(event) {
         event.preventDefault();
         scrollSection();
+    });
+
+    participate.on('click' ,function(event) {
+        event.preventDefault();
+        participateOn();
     });
 
     //Functions
@@ -27,6 +34,13 @@ app.view('header', ['$el'], function ($el) {
         var nextPosition = siblingSection.offset().top;
         $( 'html, body' ).stop().animate({
             'scrollTop': nextPosition
+        }, 900, 'swing');
+    }
+
+    function participateOn() {
+        var participateGoalPos = participateGoal.offset().top;
+        $( 'html, body' ).stop().animate({
+            'scrollTop': participateGoalPos
         }, 900, 'swing');
     }
 
