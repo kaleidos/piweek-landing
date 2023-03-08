0c97219d9246082bd2ff26502b6b14a395efd5c0
David Barragán Merino <david.barragan@kaleidos.net>
2023-03-08 14:35:14 +0100

    chore: update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
 /*global require*/
 "use strict";
 
-var gulp = require('gulp'),
-  path = require('path'),
-  data = require('gulp-data'),
-  pug = require('gulp-pug'),
-  prefix = require('gulp-autoprefixer'),
-  sass = require('gulp-sass'),
-  browserSync = require('browser-sync');
+const gulp = require('gulp'),
+      path = require('path'),
+      pug = require('gulp-pug'),
+      prefix = require('gulp-autoprefixer'),
+      sass = require('gulp-sass')(require('sass')),
+      browserSync = require('browser-sync');
 
 /*
  * Directories here
  */
-var paths = {
+const paths = {
   dist: './dist/',
   sass: './src/sass/',
   css: './dist/css/',
@@ -23,81 +22,97 @@ var paths = {
 /**
  * Compile .pug files
  */
-gulp.task('pug', function () {
+function buildHtml () {
   return gulp.src('./src/*.pug')
     .pipe(pug())
     .on('error', (err) => {
       console.log(err.message);
     })
     .pipe(gulp.dest(paths.dist));
-});
-
-/**
- * Recompile .pug files and live reload the browser
- */
-gulp.task('rebuild', ['pug'], function () {
-  browserSync.reload();
-});
-
-/**
- * Wait for pug and sass tasks, then launch the browser-sync Server
- */
-gulp.task('browser-sync', ['sass', 'pug'], function () {
-  browserSync({
-    server: {
-      baseDir: paths.dist
-    },
-    notify: false
-  });
-});
+}
 
 /**
  * Compile .scss files into dist css directory With autoprefixer no
  * need for vendor prefixes then live reload the browser.
  */
-gulp.task('sass', function () {
+function buildCss () {
   return gulp.src(paths.sass + '*.scss')
     .pipe(sass({
       includePaths: [paths.sass],
       outputStyle: 'compressed'
     }))
     .on('error', sass.logError)
-    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+    .pipe(prefix(['last 15 versions', '> 1%'], {
       cascade: true
     }))
     .pipe(gulp.dest(paths.css))
     .pipe(browserSync.reload({
       stream: true
     }));
-});
+}
 
 /** Copy images to dist **/
-gulp.task('copyimages', function() {
-   gulp.src('./src/images/**/*.{jpg,gif,png,svg}')
-   .pipe(gulp.dest(paths.images));
-});
+function copyImages() {
+  return gulp.src('./src/images/**/*.{jpg,gif,png,svg}')
+    .pipe(gulp.dest(paths.images));
+}
 
 /** Copy js to dist **/
-gulp.task('copyjs', function() {
-   gulp.src('./src/js/**/*')
-   .pipe(gulp.dest(paths.jsfiles));
-});
+function copyJs() {
+  return gulp.src('./src/js/**/*')
+    .pipe(gulp.dest(paths.jsfiles));
+}
+
+
+/** Start browser-sync Server **/
+function browserSyncServer(cb) {
+  gulp.parallel(buildCss, buildHtml, copyImages, copyJs);
+  browserSync({
+    server: {
+      baseDir: paths.dist
+    },
+    notify: false
+  });
+  cb();
+}
+
+/** Reload browser-sync **/
+function browserSyncReload(cb){
+  browserSync.reload();
+  cb();
+}
 
 /**
- * Watch scss files for changes & recompile
- * Watch .pug files run pug-rebuild then reload BrowserSync
+ * Watch .scss files for changes & recompile
+ * Watch .pug files for changes & recompile
  */
-gulp.task('watch', function () {
-  gulp.watch(paths.sass + '**/*.scss', ['sass']);
-  gulp.watch('./src/**/*.pug', ['rebuild']);
-});
+function watch () {
+  gulp.watch(
+    paths.sass + '**/*.scss',
+    gulp.series(buildCss, browserSyncReload)
+  );
+  gulp.watch(
+    './src/**/*.pug',
+    gulp.series(buildHtml, browserSyncReload)
+  );
+}
 
-// Build task compile sass and pug.
-gulp.task('build', ['sass', 'pug', 'copyimages', 'copyjs']);
 
-/**
- * Default task, running just `gulp` will compile the sass,
- * compile the jekyll site, launch BrowserSync then watch
- * files for changes
- */
-gulp.task('default', ['browser-sync', 'watch']);
+// Build task compile sass and pug and copy images and css.
+const build = gulp.parallel(
+  buildCss,
+  buildHtml,
+  copyImages,
+  copyJs
+);
+
+// Serve (for developers)
+const serve = gulp.series(
+  build,
+  browserSyncServer,
+  watch
+);
+
+
+exports.build = build;
+exports.default = serve;
